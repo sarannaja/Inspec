3ea7012fad3da720a686ce3c702e73021ef7b2ea
tungman08 <tungman08@gmail.com>
2020-02-13 15:32:06 +0700

    fix controller

--- InspecWeb/Program.cs
+++ InspecWeb/Program.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using InspecWeb.Data;
 
 namespace InspecWeb
 {
