4f590fa38e60fbce411314ef7ea1dbc05400f707
sarannaja <s.sarannaja@gmail.com>
2020-02-13 09:46:42 +0700

    p' tungman

--- InspecWeb/Program.cs
+++ InspecWeb/Program.cs
@@ -14,7 +14,7 @@ namespace InspecWeb
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().MigrateDatabase().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
