5a929e4b3e74a0558c2f43c002d830bec2dc8d78
Song <panukorn299@gmail.com>
2020-04-20 13:59:41 +0700

    electronic book

---
+++ InspecWeb/ClientApp/src/app/electronic-book/create-electronic-book/create-electronic-book.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-electronic-book',
+  templateUrl: './create-electronic-book.component.html',
+  styleUrls: ['./create-electronic-book.component.css']
+})
+export class CreateElectronicBookComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ InspecWeb/ClientApp/src/app/electronic-book/electronic-book.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-electronic-book',
+  templateUrl: './electronic-book.component.html',
+  styleUrls: ['./electronic-book.component.css']
+})
+export class ElectronicBookComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
