173a2debac49ecd480951badb08c2ee2a0ce25e8
Song <panukorn299@gmail.com>
2020-04-24 11:29:12 +0700

    crud electronic book

---
+++ InspecWeb/ClientApp/src/app/electronic-book/detail-electronic-book/detail-electronic-book.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-electronic-book',
+  templateUrl: './detail-electronic-book.component.html',
+  styleUrls: ['./detail-electronic-book.component.css']
+})
+export class DetailElectronicBookComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ InspecWeb/ClientApp/src/app/electronic-book/edit-electronic-book/edit-electronic-book.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-electronic-book',
+  templateUrl: './edit-electronic-book.component.html',
+  styleUrls: ['./edit-electronic-book.component.css']
+})
+export class EditElectronicBookComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
