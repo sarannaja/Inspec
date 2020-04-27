bd6c0c4812352d3bd7afc3714fd841fa8b2e7f2f
sarannaja <s.sarannaja@gmail.com>
2020-04-27 11:19:07 +0700

    12345

---
+++ InspecWeb/Models/SubquestionGroup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace InspecWeb.Models
+{
+    [Table("SubquestionGroups")]
+    [Description("ตารางเชื่อมนโยบาลกลาง")]
+    public class SubquestionGroup
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Description("PK")]
+        public long Id { get; set; }
+
+        [ForeignKey("CentralPolicyProvince")]
+        [Description("FK: จังหวัด")]
+        public long CentralPolicyProvinceId { get; set; }
+        public virtual CentralPolicyProvince CentralPolicyProvince { get; set; }
+
+        [ForeignKey("Subquestion")]
+        [Description("FK: คำถามย่อย")]
+        public long SubquestionId { get; set; }
+        public virtual Subquestion Subquestion { get; set; }
+
+        [ForeignKey("ProvincialDepartment")]
+        [Description("FK: ProvincialDepartment")]
+        public long ProvincialDepartmentId { get; set; }
+        public virtual ProvincialDepartment ProvincialDepartment { get; set; }
+    }
+}
