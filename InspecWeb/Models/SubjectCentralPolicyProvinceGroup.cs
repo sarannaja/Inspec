2f6d91185e1160f84a4430661f1480fedc106c8a
sarannaja <s.sarannaja@gmail.com>
2020-04-28 14:21:28 +0700

    1324

---
+++ InspecWeb/Models/SubjectCentralPolicyProvinceGroup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace InspecWeb.Models
+{
+    [Table("SubjectCentralPolicyProvinceGroups")]
+    [Description("ตารางเชื่อมนโยบาลกลาง")]
+    public class SubjectCentralPolicyProvinceGroup
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Description("PK")]
+        public long Id { get; set; }
+
+        [ForeignKey("SubjectCentralPolicyProvince")]
+        [Description("FK: ประเด็น")]
+        public long SubjectCentralPolicyProvinceId { get; set; }
+        public virtual SubjectCentralPolicyProvince SubjectCentralPolicyProvince { get; set; }
+
+        [ForeignKey("ProvincialDepartment")]
+        [Description("FK: ประเด็น")]
+        public long ProvincialDepartmentId { get; set; }
+        public virtual ProvincialDepartment ProvincialDepartment { get; set; }
+    }
+}
