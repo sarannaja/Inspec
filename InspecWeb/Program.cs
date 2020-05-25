f2eca2627218e1806618af85ad6ff2420975cbdc
sarannaja <s.sarannaja@gmail.com>
2020-05-26 02:19:41 +0700

    template fix

--- InspecWeb/Program.cs
+++ InspecWeb/Program.cs
@@ -11,20 +11,17 @@ namespace InspecWeb
 {
     public class Program
     {
+        
         public static void Main(string[] args)
-        {
+        { 
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+         public static IHostBuilder CreateHostBuilder(string[] args) =>
            Host.CreateDefaultBuilder(args)
-        .ConfigureWebHostDefaults(webBuilder =>
-        {
-            webBuilder.ConfigureKestrel(serverOptions =>
-            {
-                // Set properties and call methods on options
-            })
-            .UseStartup<Startup>();
-        });
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
