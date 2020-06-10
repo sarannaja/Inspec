afcc45a38334ff843c1e3d21da037fe9dbc1962f
k12club <k12club@hotmail.com>
2020-06-10 08:45:48 +0700

    report nik

--- InspecWeb/ClientApp/src/app/report/report-subject/report-subject.component.ts
+++ InspecWeb/ClientApp/src/app/report/report-subject/report-subject.component.ts
@@ -8,6 +8,10 @@ import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { ReportService } from 'src/app/services/report.service';
 import { centralPolicyProvinces, subjectCentralPolicyProvinceGroups } from '../../models/reportnik';
 
+import * as Excel from "exceljs/dist/exceljs.min.js";
+import * as ExcelProper from "exceljs";
+import * as fs from 'file-saver';
+
 @Component({
   selector: 'app-report-subject',
   templateUrl: './report-subject.component.html',
@@ -58,7 +62,8 @@ export class ReportSubjectComponent implements OnInit {
   test(id) {
     //console.log(id);
     var mapData: Array<any> = []
-    this.reportservice.getreportsubject(id).subscribe(result => {
+    var dataEx: Array<any> = []
+    this.reportservice.getreportsubject(id).subscribe(async result => {
       //console.log("report", result);
       var title = result.title
       var centralPolicyProvinces: Array<centralPolicyProvinces> = result.centralPolicyProvinces
@@ -75,7 +80,7 @@ export class ReportSubjectComponent implements OnInit {
       }
 
 
-      centralPolicyProvinces.forEach(item => {
+      await centralPolicyProvinces.forEach(item => {
         mapData.push(
           item.subjectCentralPolicyProvinces
             .filter(item => {
@@ -112,10 +117,124 @@ export class ReportSubjectComponent implements OnInit {
                 }
               })
         )
-        console.log("mapData", mapData);
       })
+      this.exportExcel2(mapData.map((result, index) => {
+        if (result.lenght != 0) {
+          console.log(result);
+          return result
+        }
+        
+      }).map(result => {
+        
+        
+        return [Object.values({
+          title: result.data.title,
+          subjectCentralPolicyProvinces: result.data.subjectCentralPolicyProvinces,
+          department: result.data.department,
+          province: result.data.province,
+          status: result.data.status
+        })]
+      }), mapData[0].column)
+      await mapData.forEach((element, index) => {
+        console.log("mapData", element);
+        dataEx.push(element.value)
+
+
+
+      });
+
+      // this.exportExcel2(mapData.value,mapData.column)
 
     })
+  }
+  exportExcel2(data: Array<any>, column) {
+    // Excel Title, Header, Data
+    const title = 'ทะเบียนรายงานผลการตรวจราชการ';
+    // const header = ['Year', 'Month', 'Make', 'Model', 'Quantity', 'Pct'];
+    const header = [
+      'ลำดับที่',
+      'วัน/เดือน/ปี ที่มีรายงาน',
+      'ประเด็น/เรื่อง', 'จังหวัด', 'ผู้สร้างรายงาน',
+      'สถานะรายงาน',
+      'วัน/เดือน/ปี ที่ส่งรายงาน',
+      'ข้อสั่งการของผู้บังคับบัญชา (มี/ไม่มี)',
+      'วัน/เดือน/ปี ที่มีข้อสั่งการ',
+      'ไฟล์/เอกสารแนบ (เอกสาร/ภาพ/เสียง/วีดิทัศน์)'
+    ];
+    // Create workbook and worksheet
+    // const workbook = new Workbook();
+    let workbook: ExcelProper.Workbook = new Excel.Workbook();
+    let worksheet = workbook.addWorksheet('Sales Data');
+    // Add Row and formatting
+    const titleRow = worksheet.addRow([title]);
+    titleRow.font = { name: 'Comic Sans MS', family: 4, size: 16, underline: 'double', bold: true };
+    worksheet.addRow([]);
+    // Add Image
+    // const logo = workbook.addImage({
+    //     //   base64: logoFile.logoBase64,
+    //     extension: 'png',
+    // });
+
+    // worksheet.addImage(logo, 'E1:F3');
+    worksheet.mergeCells('A1:F2');
+    // worksheet.mergeCells('G5:G8');
+
+    // Blank Row
+    worksheet.addRow([]);
+
+    // Add Header Row
+    const headerRow = worksheet.addRow(column);
+
+    // Cell Style : Fill and Border
+    headerRow.eachCell((cell, number) => {
+      cell.fill = {
+        type: 'pattern',
+        pattern: 'solid',
+        fgColor: { argb: 'FFFFFF' },
+        bgColor: { argb: 'FFFFFF' }
+      };
+      cell.border = { top: { style: 'thin' }, left: { style: 'thin' }, bottom: { style: 'thin' }, right: { style: 'thin' } };
+    });
+    // worksheet.addRows(data);
+
+
+    // Add Data and Conditional Formatting
+    data.forEach(d => {
+      const row = worksheet.addRow(d);
+      const qty = row.getCell(5);
+      // let color = 'FFFFFF';
+      // if (+qty.value < 500) {
+      //   color = 'FFFFFF';
+      // }
+      // qty.fill = {
+      //   type: 'pattern',
+      //   pattern: 'solid',
+      //   fgColor: { argb: color }
+      // };
+    });
+
+    // worksheet.getColumn(3).width = 30;
+    // worksheet.getColumn(4).width = 30;
+    // worksheet.addRow([]);
+
+    // Footer Row
+    // const footerRow = worksheet.addRow(['This is system generated excel sheet.']);
+    // footerRow.getCell(1).fill = {
+    //     type: 'pattern',
+    //     pattern: 'solid',
+    //     fgColor: { argb: 'FFFFFF' }
+    // };
+    // footerRow.getCell(1).border = { top: { style: 'thin' }, left: { style: 'thin' }, bottom: { style: 'thin' }, right: { style: 'thin' } };
+
+    // // Merge Cells
+    // worksheet.mergeCells(`A${footerRow.number}:F${footerRow.number}`);
+
+    // Generate Excel File with given name
+    workbook.xlsx.writeBuffer().then((data) => {
+      let blob = new Blob([data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+      fs.saveAs(blob, title + '.xlsx');
+    })
+    // const subTitleRow = worksheet.addRow(['Date : ' + this.datePipe.transform(new Date(), 'medium')]);
   }
   openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
