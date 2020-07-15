4af1a5c5ec153267d6526e217ed8e2bba5088619
k12club <k12club@hotmail.com>
2020-07-16 01:08:51 +0700

    fixed

--- InspecWeb/Data/ApplicationDbContext.cs
+++ InspecWeb/Data/ApplicationDbContext.cs
@@ -122,8 +122,13 @@ namespace InspecWeb.Data
         public DbSet<Log> Logs { get; set; }
         public DbSet<CentralPolicyProvinceEvent> CentralPolicyProvinceEvents { get; set; }
         public DbSet<SubjectGroup> SubjectGroups { get; set; }
-        public DbSet<ElectronicBookInvite> ElectronicBookInvites { get; set; }  
+
+        public DbSet<ElectronicBookInvite> ElectronicBookInvites { get; set; }
+
         public DbSet<CentralPolicyEventQuestion> CentralPolicyEventQuestions { get; set; }
+        public DbSet<ElectronicBookProvinceApproveFile> ElectronicBookProvinceApproveFiles { get; set; }
+        public DbSet<AnswerSubquestionStatus> AnswerSubquestionStatuses { get; set; }
+
         public DbSet<ExecutiveOrderAnswer> ExecutiveOrderAnswers { get; set; }
         public DbSet<ExecutiveOrderAnswerDetail> ExecutiveOrderAnswerDetails { get; set; }
         public DbSet<RequestOrderAnswer> RequestOrderAnswers { get; set; }
