2d46d1f5538ccaec95cb4e1f0975aa9c186b73f2
yochigang OMaZ๏์N <yochigang@hotmail.com>
2020-08-05 10:35:25 +0700

    yochigang20200805

--- InspecWeb/ClientApp/src/app/services/subdistrict.service.ts
+++ InspecWeb/ClientApp/src/app/services/subdistrict.service.ts
@@ -17,4 +17,19 @@ export class SubdistrictService {
     
     return this.http.get(this.url+id)
   }
+  addsubdistrict(supdistrictData,provinceId) {
+    const formData = new FormData();
+    formData.append('DistrictId', provinceId);
+    formData.append('Name', supdistrictData.Name);
+    return this.http.post(this.url, formData);
+  }
+  deletesupdistrict(id) {
+    return this.http.delete(this.url + id);
+  }
+  editsupdistrict(supdistrictData,id) {
+    const formData = new FormData();
+    formData.append('Name', supdistrictData.Name);
+   
+    return this.http.put(this.url+id, formData);
+  }
 }
