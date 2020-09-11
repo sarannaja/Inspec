ab85b8190689417a7996e4ce80589da516942ebb
sarannaja <s.sarannaja@gmail.com>
2020-09-11 15:35:30 +0700

    deploy

--- deploy/routes/deploy.js
+++ deploy/routes/deploy.js
@@ -4,8 +4,31 @@ const nodeCmd = require("node-cmd");
 /* GET home page. */
 router.post("/", function (req, res, next) {
   let command = req.body.command
-  nodeCmd.get(command,(err, data, stderr) => res.json(data)
+  let gitpull = req.body.gitpull
+  let tofol = `
+  cd C:\Inspec\InspecWeb
+  `
+  let pull = `git pull
+  `
+  let deployProd = `
+  dotnet publish InspecWeb.csproj -o publish
+echo "publish success"
+iisreset /start
+echo "start server
+  `
+
+  nodeCmd.get(gitpull ?tofol+pull+deployProd : tofol+deployProd , (err, data, stderr) => res.json(data)
     // res.render("index", { title: data.toString() })
+  );
+});
+
+router.get("/", function (req, res, next) {
+  let command = req.body.command
+
+
+  nodeCmd.get(command, (err, data, stderr) =>
+    //  res.json(data)
+    res.render("index", { title: data.toString() })
   );
 });
 
