0c1465f60d5b1084030dc3953095ccf04538a116
Palm <k12club@hotmail.com>
2020-09-11 15:48:47 +0700

    fix deploy

--- deploy/routes/deploy.js
+++ deploy/routes/deploy.js
@@ -6,19 +6,24 @@ router.post("/", function (req, res, next) {
   let command = req.body.command
   let gitpull = req.body.gitpull
   let tofol = `
-  cd C:\Inspec\InspecWeb
-  `
-  let pull = `git pull
-  `
-  let deployProd = `
+  cd C:\\Inspec\\InspecWeb
+  git pull
   iisreset /stop
   dotnet publish InspecWeb.csproj -o publish
   iisreset /start
   `
-
-  nodeCmd.get(gitpull ?tofol+pull+deployProd : tofol+deployProd , (err, data, stderr) => res.json(data)
-    // res.render("index", { title: data.toString() })
-  );
+  let pull = `git pull
+  `
+  let deployProd = `
+  cd C:\\Inspec\\InspecWeb
+  iisreset /stop
+  dotnet publish InspecWeb.csproj -o publish
+  iisreset /start
+  `
+  res.json(tofol)
+  // nodeCmd.get(tofol , (err, data, stderr) => res.json(data)
+  //   // res.render("index", { title: data.toString() })
+  // );
 });
 
 router.get("/", function (req, res, next) {
