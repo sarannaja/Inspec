cf4a78b13017be81e03fc0d7b2cb729e2f522dce
k12club <k12club@hotmail.com>
2020-09-30 01:08:25 +0700

    5

--- InspecWeb/ClientApp/gulpfile.js
+++ InspecWeb/ClientApp/gulpfile.js
@@ -1,9 +1,9 @@
-// const { src, dest } = require('gulp');
-// var replace = require('gulp-replace')
+const { src, dest } = require('gulp');
+var replace = require('gulp-replace')
 
-// exports.default = function (cb) {    
-//     src(['dist/index.html'])
-//         .pipe(replace('type="module"', 'type="text/javascript"'))
-//         .pipe(dest('dist/'));
-//     cb();
-// };
+exports.default = function (cb) {    
+    src(['dist/index.html'])
+        .pipe(replace('type="module"', 'type="text/javascript"'))
+        .pipe(dest('dist/'));
+    cb();
+};
--- InspecWeb/ClientApp/src/polyfills.ts
+++ InspecWeb/ClientApp/src/polyfills.ts
@@ -59,7 +59,7 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * Load `$localize` onto the global scope - used if i18n tags appear in Angular templates.
  */
-import '@angular/localize/init';
+// import '@angular/localize/init'; 
 // import 'core-js/es7/reflect';
 /***************************************************************************************************
  * APPLICATION IMPORTS
