dc4076a281789859aee6d11744b1266e33ea7a23
sarannaja <s.sarannaja@gmail.com>
2020-09-30 00:10:43 +0700

    s

--- InspecWeb/ClientApp/src/polyfills.ts
+++ InspecWeb/ClientApp/src/polyfills.ts
@@ -56,7 +56,10 @@
  * Zone JS is required by default for Angular itself.
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
-
+/***************************************************************************************************
+ * Load `$localize` onto the global scope - used if i18n tags appear in Angular templates.
+ */
+import '@angular/localize/init';
 // import 'core-js/es7/reflect';
 /***************************************************************************************************
  * APPLICATION IMPORTS
