b91afd751e9c1bd84b37389b8c46bda5368b0364
k12club <k12club@hotmail.com>
2020-10-21 02:46:57 +0700

    save

--- InspecWeb/Controllers/UtinityController.cs
+++ InspecWeb/Controllers/UtinityController.cs
@@ -10,37 +10,46 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Quartz;
 
-namespace InspecWeb.Controllers {
-    [Route ("api/[controller]")]
+namespace InspecWeb.Controllers
+{
+    [Route("api/[controller]")]
     [ApiController]
-    public class UtinityController : ControllerBase {
+    public class UtinityController : ControllerBase
+    {
         private readonly IMailService mailService;
-        public UtinityController (
+        public UtinityController(
             IMailService mailService
-        ) {
+        )
+        {
             this.mailService = mailService;
         }
 
         // POST: ตัวอย่างการส่ง email 
-        [HttpPost ("send")]
-        public async Task<IActionResult> SendMail ([FromForm] MailRequest request) {
-            try {
-                var send = new MailRequest {
+        [HttpPost("send")]
+        public async Task<IActionResult> SendMail([FromForm] MailRequest request)
+        {
+            try
+            {
+                var send = new MailRequest
+                {
                     ToEmail = "k12club@hotmail.com",
                     Subject = "Test Subject",
                     Body = "Test Body"
                 };
-                await mailService.SendEmailAsync (request);
-                return Ok ();
-            } catch (Exception ex) {
+                await mailService.SendEmailAsync(request);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
 
                 throw;
             }
 
         }
         //cronjob เดี๋ยยวค่อยอธิบาย
-        public void Process () {
-            Console.WriteLine ("ta kai yung hen log nee is saran yung mai dai tum cronjob 5555+ as " + DateTime.Now.ToString ("F"));
+        public void Process()
+        {
+            Console.WriteLine("ta kai yung hen log nee is saran yung mai dai tum cronjob 5555+ as " + DateTime.Now.ToString("F"));
         }
 
     }
