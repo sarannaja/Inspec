76bcbcbf017bbc1584a618fa3d8532eaf9ab7105
k12club <k12club@hotmail.com>
2020-10-29 10:07:54 +0700

    save

--- InspecWeb/Models/TrainingProgram.cs
+++ InspecWeb/Models/TrainingProgram.cs
@@ -23,7 +23,6 @@ namespace InspecWeb.Models
         [Description("FK: ช่วงหลักสูตรอบรม")]
         public long TrainingPhaseId { get; set; }
 
-        public virtual TrainingProgramFile TrainingProgramFiles { get; set; }
         public virtual TrainingPhase TrainingPhase { get; set; }
         public virtual TrainingProgramLoginQRCode TrainingProgramLoginQRCodes { get; set; }
 
