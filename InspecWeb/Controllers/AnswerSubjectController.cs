f8b10aa621c6773762450c7702f5186a9e4d404f
Nik400cc <tanapol868@gmail.com>
2020-11-13 15:53:44 +0700

    edit13112

--- InspecWeb/Controllers/AnswerSubjectController.cs
+++ InspecWeb/Controllers/AnswerSubjectController.cs
@@ -257,6 +257,8 @@ namespace InspecWeb.Controllers
             var centralpolicydata = _context.CentralPolicyUsers
                 .Include(m => m.CentralPolicy)
                 .ThenInclude(m => m.Typeexaminationplan)
+                .Include(m => m.CentralPolicy)
+                .ThenInclude(m => m.CentralPolicyDates)
                 .OrderByDescending(m => m.Id)
                 .Where(m => m.InspectionPlanEvent.Status == "ใช้งานจริง")
                 .Where(m => m.Status == "ตอบรับ")
@@ -375,6 +377,7 @@ namespace InspecWeb.Controllers
             var question = _context.CentralPolicyEventQuestions
                 .Include(m => m.CentralPolicyEvent)
                 .ThenInclude(m => m.CentralPolicy)
+                .ThenInclude(m => m.CentralPolicyDates)
                 .Include(m => m.CentralPolicyEvent)
                 .ThenInclude(m => m.InspectionPlanEvent)
                 .ThenInclude(m => m.Province)
