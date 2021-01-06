f34c5713ade6121be68b78d83487af3840d01859
APToey <fantasy_tey@hotmail.com>
2021-01-06 17:58:24 +0700

    Update_20210106_1758

--- InspecWeb/ClientApp/src/app/training-programlogin/training-programlogin.module.ts
+++ InspecWeb/ClientApp/src/app/training-programlogin/training-programlogin.module.ts
@@ -1,16 +1,18 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TrainingProgramLoginComponent } from './training-programlogin.component';
 import { RouterModule, Routes } from '@angular/router';
 import { QRCodeModule } from 'angularx-qrcode';
 import { SharingModule } from '../sharing/sharing.module';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthorizeInterceptor } from 'src/api-authorization-new/authorize.interceptor';
 
 const routes: Routes = [
-  {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: '/'
-  },
+  // {
+  //   path: '',
+  //   pathMatch: 'full',
+  //   redirectTo: '/'
+  // },
   {
     path: '',
     component: TrainingProgramLoginComponent
@@ -23,8 +25,15 @@ const routes: Routes = [
     RouterModule.forChild(routes),
     CommonModule,
     QRCodeModule,
-    SharingModule
+    SharingModule,
+
+    HttpClientModule,
   ],
+  // providers: [
+  //   { provide: HTTP_INTERCEPTORS, useClass: AuthorizeInterceptor, multi: true },
+
+  // ],
+  // schemas: [CUSTOM_ELEMENTS_SCHEMA],
   exports: [
     RouterModule
   ]
