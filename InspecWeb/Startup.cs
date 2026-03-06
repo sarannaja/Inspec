c87b52c8513d89fe7a0da6e2649c7b78de67effd
Panukorn <panukorn299@gmail.com>
2026-03-06 20:32:53 +0700

    fix again 5.

--- InspecWeb/Startup.cs
+++ InspecWeb/Startup.cs
@@ -199,7 +199,7 @@ namespace InspecWeb
                     context.Response.Headers["Content-Security-Policy"] =
                         "default-src 'self'; " +
                         "script-src 'self' 'unsafe-inline' 'unsafe-eval'; " +
-                        "style-src 'self' https://fonts.googleapis.com https://cdnjs.cloudflare.com; " +
+                        "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com; " +
                         "font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com; " +
                         "img-src 'self' data: blob:; " +
                         "connect-src 'self'; " +
